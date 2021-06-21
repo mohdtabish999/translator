59d3f2aa0c1f6aaf5e7c3f6ce84a0e5fae54c435
mohdtabish999 <mohdtabish999@gmail.com>
2021-06-21 12:04:46 +0530

    added config refresh annotation

--- src/main/java/com/example/translator/TranslatorApplication.java
+++ src/main/java/com/example/translator/TranslatorApplication.java
@@ -1,8 +1,15 @@
 package com.example.translator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +17,20 @@ public class TranslatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TranslatorApplication.class, args);
+	}
+	
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+
+	  @Value("${message:Config Server is not working. Please check...}")
+	  private String message;
+
+	  @RequestMapping("/message")
+	  String getMessage() throws JsonMappingException, JsonProcessingException {
+		  System.out.println(this.message);
+	    return this.message;
+	  }
 	}
 
 }
--- src/main/java/com/example/translator/controller/TranslatorController.java
+++ src/main/java/com/example/translator/controller/TranslatorController.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.example.translator.service.TranslatorServiceXmlJson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
+@RefreshScope
 @RequestMapping("/translate")
 public class TranslatorController {
 	
