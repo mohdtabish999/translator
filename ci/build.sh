88c20d828555684f7f16d62594d24bfe7b77cc3f
mohdtabish999 <mohdtabish999@gmail.com>
2021-06-28 12:10:24 +0530

    added certificate command

--- ci/build.sh
+++ ci/build.sh
@@ -11,4 +11,7 @@ cd ..
 #list all files
 jar tf source-code/target/*.jar
 
+#Instead of .profile, lets add certificate from here and check ?
+$HOME/.java-buildpack/open_jdk_jre/bin/keytool -importcert -trustcacerts -noprompt -keystore $HOME/.java-buildpack/open_jdk_jre/lib/security/cacerts -storepass changeit  -alias MyCert -file $HOME/BOOT-INF/classes/base64_hclcnlabs_certificate.cer
+
 cp source-code/target/*.jar  build-output/.
