74a58b2394e6325912dd5e474a787872d26cb8ee
mohdtabish999 <mohdtabish999@gmail.com>
2021-07-08 14:03:37 +0530

    added oauth resource server properties

--- src/main/java/com/example/translator/config/BeanConfig.java
+++ src/main/java/com/example/translator/config/BeanConfig.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -12,7 +15,7 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
 
 @Configuration
-public class BeanConfig {
+public class BeanConfig extends WebSecurityConfigurerAdapter {
 	
 	  @Bean public ViewResolver contentNegotiatingViewResolver() {
 	  ContentNegotiatingViewResolver resolver = new
@@ -24,5 +27,26 @@ public class BeanConfig {
 
 	  resolver.setDefaultViews(views); return resolver; 
 	  }
+	  
+	  @Override
+		protected void configure(HttpSecurity http) throws Exception {
+			
+			/*
+			 * http.authorizeRequests(authz ->
+			 * authz.antMatchers("/foos/**").hasAuthority("SCOPE_message.read")
+			 * .antMatchers(HttpMethod.POST,
+			 * "/foos").hasAuthority("SCOPE_message.write").anyRequest().authenticated())
+			 * .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+			 */
+
+			http.csrf().disable().authorizeRequests().antMatchers("/", "/error**").permitAll().anyRequest()
+					.authenticated().and().oauth2ResourceServer().jwt();
+
+				/*
+				 * http .mvcMatcher("/foos/**") .authorizeRequests()
+				 * .mvcMatchers("/foos/**").access("hasAuthority('SCOPE_message.read')") .and()
+				 * .oauth2ResourceServer() .jwt();
+				 */
+		}
 	 
 }
